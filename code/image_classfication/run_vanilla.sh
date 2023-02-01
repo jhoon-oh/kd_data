f921e6de4d31b96cc846a13653537295c104d9a6
TufailRouf <121852083+TufailRouf@users.noreply.github.com>
2023-02-01 08:35:14 +0000

    Update run_vanilla.sh

--- code/image_classfication/run_vanilla.sh
+++ code/image_classfication/run_vanilla.sh
@@ -4,7 +4,7 @@
 
 python main.py --device=cuda:0 \
                --student=wrn-28-2 \
-               --dataset=cifar100 \
+               --dataset=cifar10 \
                --batch_size=128 \
                --num_epochs=200 \
                --mode=crop \
